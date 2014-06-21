33eeb1bd20b5b973b412030c6326b0ebd83678ad
Mathias Sunardi <m.sunardi@gmail.com>
2014-06-21 11:32:07 -0700

    Added home view

--- bundlerco/cause/tests.py
+++ bundlerco/cause/tests.py
@@ -1,5 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from django_dynamic_fixture import G
 
 from cause.models import Cause
@@ -15,3 +18,9 @@ class HomeTest(TestCase):
 	def test_home_url(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home)
+
+	def test_home_html(self):
+		request = HttpRequest()
+		response = home(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
