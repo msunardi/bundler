bfe9ba793a38b870ab1275a2b3af927b2e4ab51b
Mathias Sunardi <m.sunardi@gmail.com>
2014-06-21 18:07:19 -0700

    Temporary fix: get url resolver working

--- bundlerco/bundlerco/urls.py
+++ bundlerco/bundlerco/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cause.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('cause.urls', namespace='cause')),
+    url(r'^cause/', include('cause.urls', namespace='cause')),
     url(r'^admin/', include(admin.site.urls)),
 )
