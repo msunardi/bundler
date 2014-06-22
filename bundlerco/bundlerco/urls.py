61afa5debeebf1675634919fb54d75dce73ac2a4
Mathias Sunardi <m.sunardi@gmail.com>
2014-06-22 13:11:10 -0700

    Added link to pledge page

--- bundlerco/bundlerco/urls.py
+++ bundlerco/bundlerco/urls.py
@@ -20,5 +20,6 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     	url(r'^profile_images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.PROFILE_IMAGES_ROOT}),
    )
--- bundlerco/cause/urls.py
+++ bundlerco/cause/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
 	url(r'^edit/(?P<pk>\d+)/$', views.CauseEditView.as_view(), name='edit'),
 	url(r'^create/$', views.CauseCreateView.as_view(), name='create'),
 	url(r'^createsuccess/$', views.CauseCreateSuccessView.as_view(), name='create-success'),
+
+	url(r'^pledge/(?P<pk>\d+)/$', views.PledgeView.as_view(), name='pledge'),
 )
