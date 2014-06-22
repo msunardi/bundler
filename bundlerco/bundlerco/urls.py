04f7fa5b39d84ecf8e2c1a143a26818830f5bede
Mathias Sunardi <m.sunardi@gmail.com>
2014-06-21 23:58:20 -0700

    Added How Bundler works template

--- bundlerco/bundlerco/urls.py
+++ bundlerco/bundlerco/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cause.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^cause/', include('cause.urls', namespace='cause')),
+ 	url(r'^how/', TemplateView.as_view(template_name="how.html"), name='how'),
     url(r'^admin/', include(admin.site.urls)),
 )
